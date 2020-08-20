776984ddd18bbaf04d19bd339c234640652bbd30
Rodrigo Rahman <rodrigorahman@gmail.com>
2020-08-20 00:08:05 -0300

    acertos

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -7,9 +7,6 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-      ),
       body: BlocProvider(
         create: (context) => Modular.get<AuthCubit>()..checkUserLogged(),
         child: SplashContentPage(),
